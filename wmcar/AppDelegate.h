f6044587ec4fd91e35cbdb46d86652684f4e3755
AdaZhao1211 <adazhao1211@gmail.com>
2016-11-20 12:21:40 -0500

    Initial Commit

---
+++ wmcar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wmcar
+//
+//  Created by Ada on 11/20/16.
+//  Copyright © 2016 Ada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
