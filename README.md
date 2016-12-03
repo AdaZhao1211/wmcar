fad85b8663b0db230bc2c3809d1a2b7ed4807e8c
AdaZhao1211 <adazhao1211@gmail.com>
2016-12-03 17:03:45 -0500

    directions

--- README.md
+++ README.md
@@ -11,8 +11,14 @@ You can also enjoy the WMCar on your Apple Watch. You can pinpoint your location
 [CSCI-UA.0480-002 iOS Programming] (http://cs.nyu.edu/courses/fall16/CSCI-UA.0480-002/)
 
 #To-do
-Direction
+pin, selected mode
+
+when close, change the button to FOUND and clear the route
+
 data between setting view and map view
+
 local memory
+
 Camera, taking photos in the note view
+
 search bar to pin the car location
--- wmcar/MapViewController.m
+++ wmcar/MapViewController.m
@@ -16,7 +16,7 @@
 @end
 
 @implementation MapViewController
-
+MKRoute *routeDetails;
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self customSetup];
@@ -58,6 +58,7 @@
     [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
 }
 
+/////////////////bottom button
 - (IBAction)setPinpoint:(id)sender {
     if([_set.titleLabel.text isEqual: @"Set Pinpoint"]){
         if(_city){
@@ -79,32 +80,75 @@
         }
     }else{
         if(_multi){
-            UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Please select a pin" message:@"You are in multi-pinpoint mode" preferredStyle:UIAlertControllerStyleAlert];
-            UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
-                                                                  handler:^(UIAlertAction * action) {}];
-            [alert addAction:defaultAction];
-            [self presentViewController:alert animated:YES completion:nil];
-            
             if(_carArray.count == 1){
                 //navigate
+                NSLog(@"in multi mode with only one pin, requeting directions");
+                MKDirectionsRequest *directionsRequest = [MKDirectionsRequest new];
+                MKPointAnnotation *temp = [_carArray objectAtIndex:0];
+                MKPlacemark *placemark = [[MKPlacemark alloc] initWithCoordinate:temp.coordinate];
+                [directionsRequest setSource:[MKMapItem mapItemForCurrentLocation]];
+                [directionsRequest setDestination:[[MKMapItem alloc] initWithPlacemark:placemark]];
+                [directionsRequest setTransportType: MKDirectionsTransportTypeWalking];
+                directionsRequest.transportType = MKDirectionsTransportTypeAutomobile;
+                MKDirections *directions = [[MKDirections alloc] initWithRequest:directionsRequest];
+                [directions calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse *response, NSError *error) {
+                    if (error) {
+                        NSLog(@"Error %@", error.description);
+                    } else {
+                        NSLog(@"no error");
+                        //got the call back
+                        routeDetails = response.routes.lastObject;
+                        [myMapView addOverlay:routeDetails.polyline];
+                    }
+                }];
             }else{
                 //alert
+                UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Please select a pin" message:@"You are in multi-pinpoint mode" preferredStyle:UIAlertControllerStyleAlert];
+                UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+                                                                      handler:^(UIAlertAction * action) {}];
+                [alert addAction:defaultAction];
+                [self presentViewController:alert animated:YES completion:nil];
             }
         }else{
-
+            NSLog(@"in non multi mode, requeting directions");
+            MKDirectionsRequest *directionsRequest = [MKDirectionsRequest new];
+            MKPointAnnotation *temp = [_carArray objectAtIndex:0];
+            MKPlacemark *placemark = [[MKPlacemark alloc] initWithCoordinate:temp.coordinate];
+            [directionsRequest setSource:[MKMapItem mapItemForCurrentLocation]];
+            [directionsRequest setDestination:[[MKMapItem alloc] initWithPlacemark:placemark]];
+            [directionsRequest setTransportType: MKDirectionsTransportTypeWalking];
+            directionsRequest.transportType = MKDirectionsTransportTypeAutomobile;
+            MKDirections *directions = [[MKDirections alloc] initWithRequest:directionsRequest];
+            [directions calculateDirectionsWithCompletionHandler:^(MKDirectionsResponse *response, NSError *error) {
+                if (error) {
+                    NSLog(@"Error %@", error.description);
+                } else {
+                    NSLog(@"no error");
+                    //got the call back
+                    routeDetails = response.routes.lastObject;
+                    [myMapView addOverlay:routeDetails.polyline];
+                }
+            }];
         }
     }
 }
 
+//overlay the route details
+-(MKOverlayRenderer *)mapView:(MKMapView *)mapView rendererForOverlay:(id<MKOverlay>)overlay {
+    MKPolylineRenderer  * routeLineRenderer = [[MKPolylineRenderer alloc] initWithPolyline:routeDetails.polyline];
+    routeLineRenderer.strokeColor = [UIColor redColor];
+    routeLineRenderer.lineWidth = 5;
+    return routeLineRenderer;
+}
 
-
+// + for multi pinpoint
 - (IBAction)addPinpoint:(id)sender {
     [_set setTitle:@"Set Pinpoint" forState:UIControlStateNormal];
     [myMapView addAnnotation:_centerAnnotation];
     _addButton.enabled = NO;
 }
 
-
+//zoom in to current location
 - (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation{
     MKMapCamera *camera = [MKMapCamera cameraLookingAtCenterCoordinate:userLocation.coordinate fromEyeCoordinate:CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude) eyeAltitude:10000];
     [mapView setCamera:camera animated:NO];
@@ -116,11 +160,14 @@
     [myMapView addAnnotation:temp];
     _centerAnnotation = temp;
 }
+
+//move the pin
 - (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated {
     _centerAnnotation.coordinate = mapView.centerCoordinate;
     _centerAnnotation.subtitle = [NSString stringWithFormat:@"%f, %f", _centerAnnotation.coordinate.latitude, _centerAnnotation.coordinate.longitude];
 }
 
+//note view
 -(IBAction)saveNote:(UIStoryboardSegue *) segue {
     MKPointAnnotation *pin = [MKPointAnnotation new];
     pin.coordinate = myMapView.centerCoordinate;
@@ -148,5 +195,27 @@
         noteVC.model = self.noteModel;
     }
 
+}
+
+//direction
+-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation{
+    // If it's the user location, just return nil.
+    if ([annotation isKindOfClass:[MKUserLocation class]])
+        return nil;
+    // Handle any custom annotations.
+    if ([annotation isKindOfClass:[MKPointAnnotation class]]) {
+        // Try to dequeue an existing pin view first.
+        MKPinAnnotationView *pinView = (MKPinAnnotationView*)[myMapView dequeueReusableAnnotationViewWithIdentifier:@"CustomPinAnnotationView"];
+        if (!pinView)
+        {
+            // If an existing pin view was not available, create one.
+            pinView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"CustomPinAnnotationView"];
+            pinView.canShowCallout = YES;
+        } else {
+            pinView.annotation = annotation;
+        }
+        return pinView;
+    }
+    return nil;
 }
 @end
